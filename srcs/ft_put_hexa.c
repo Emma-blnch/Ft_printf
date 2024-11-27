4f27ce9897c90f49ce4956ef7d83f1a1e118899a
Emma B. <emma.blnch4@gmail.com>
2024-11-27 09:37:55 +0100

    Update ft_put_hexa.c

--- srcs/ft_put_hexa.c
+++ srcs/ft_put_hexa.c
@@ -13,52 +13,52 @@
 #include "ft_printf.h"
 
 // function that counts the number of digits for printf to return
-static size_t	hexa_digits(unsigned int n)
+static size_t	count_hexa_digits(unsigned int number)
 {
 	size_t	count;
 
 	count = 0;
-	if (n == 0)
+	if (number == 0)
 		return (1);
-	while (n != 0)
+	while (number != 0)
 	{
 		count++;
-		n /= 16;
+		number /= 16;
 	}
 	return (count);
 }
 
 // function that writes our number in hexadecimal form
-static void	write_hexa(unsigned int nbr, const char format)
+static void	write_hexa_number(unsigned int number, const char format)
 {
 	char	*upper_digits;
 	char	*lower_digits;
 
 	upper_digits = "0123456789ABCDEF";
 	lower_digits = "0123456789abcdef";
-	if (nbr >= 16)
+	if (number >= 16)
 	{
-		write_hexa(nbr / 16, format);
-		write_hexa(nbr % 16, format);
+		write_hexa(number / 16, format);
+		write_hexa(number % 16, format);
 	}
 	else
 	{
 		if (format == 'x')
-			write(1, &lower_digits[nbr % 16], 1);
+			write(1, &lower_digits[number % 16], 1);
 		if (format == 'X')
-			write(1, &upper_digits[nbr % 16], 1);
+			write(1, &upper_digits[number % 16], 1);
 	}
 }
 
 
 // main function that calls our other two to write in hexadecimal and return the length of the number
-int	ft_put_hexa(unsigned int nbr, const char format)
+int	ft_put_hexa(unsigned int number, const char format)
 {
-	if (nbr == 0)
+	if (number == 0)
 	{
 		write(1, "0", 1);
 		return (1);
 	}
-	write_hexa(nbr, format);
-	return (hexa_digits(nbr));
+	write_hexa_number(number, format);
+	return (count_hexa_digits(number));
 }
