178e34f4639158ff7438f72abcf8d1d530abab39
Emma B. <emma.blnch4@gmail.com>
2024-11-27 09:34:34 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -16,52 +16,51 @@
 int	ft_check_format(va_list args, char c)
 {
 	unsigned int	nbr;
-	int				len;
+	int				formatLength;
 
-	len = 0;
-/* each if condition checks what is the argument and calls the associated function to return what is expected */
+	formatLength = 0;
 	if (c == 's') // s for string
-		len += ft_putstr(va_arg(args, char *));
+		formatLength += ft_putstr(va_arg(args, char *));
 	else if (c == 'd' || c == 'i') // d or i for a number
-		len += ft_putnbr(va_arg(args, int));
+		formatLength += ft_putnbr(va_arg(args, int));
 	else if (c == 'c') // c for a char
-		len += ft_putchar(va_arg(args, int));
+		formatLength += ft_putchar(va_arg(args, int));
 	else if (c == 'u') // u for an unsigned int
-		len += ft_put_unsigned(va_arg(args, unsigned int));
+		formatLength += ft_put_unsigned(va_arg(args, unsigned int));
 	else if (c == 'p') // p for a pointer
-		len += ft_putptr(va_arg(args, void *));
+		formatLength += ft_putptr(va_arg(args, void *));
 	else if (c == 'x' || c == 'X') // x and X for a number in hexadecimal
 	{
 		nbr = va_arg(args, unsigned int);
-		len += ft_put_hexa(nbr, c);
+		formatLength += ft_put_hexa(nbr, c);
 	}
 	else if (c == '%') // % to return a %
-		len += ft_putchar('%');
-	return (len); // we don't forget to return the length expected by printf
+		formatLength += ft_putchar('%');
+	return (formatLength);
 }
 
 // main printf function
 int	ft_printf(const char *format, ...)
 {
-	int		i;
+	int		index;
 	int		count;
 	va_list	args;
 
 	if (!format)
 		return (-1);
 	va_start(args, format);
-	i = 0;
+	index = 0;
 	count = 0;
-	while (format[i])
+	while (format[index])
 	{
-		if (format[i] == '%') // if we encounter a % sign
+		if (format[index] == '%') // if we encounter a % sign
 		{
-			count += ft_check_format(args, format[i + 1]); // we call check_format to check what is after the %
-			i++;
+			count += ft_check_format(args, format[index + 1]); // we call check_format to check what is after the %
+			index++;
 		}
 		else
-			count += ft_putchar(format[i]); // if no % then we just write what is in the position we're at
-		i++;
+			count += ft_putchar(format[index]); // if no % then we just write what is in the position we're at
+		index++;
 	}
 	va_end(args);
 	return (count);
